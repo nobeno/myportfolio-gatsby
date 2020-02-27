a2ba4c2ad1b71bf1250e9c16433c4022f7bd5f43
daichi nb <nd940501@gmail.com>
2020-02-27 21:17:33 +0900

    change sentence

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -41,23 +41,23 @@ class Child extends React.Component {
                     <dt>HTML5/CSS3</dt>
                     <dd>セブ島で2ヶ月学習。Bootstrap4を使ったサイト制作可能。</dd>
                     <dt>JavaScript</dt>
-                    <dd>セブ島で1ヶ月学習。jQuery or Reactを使用したサイト制作。</dd>
+                    <dd>セブ島で2ヶ月学習。JavaScriptの基礎とjQueryを学ぶ。Reactは独学で、Gatsbyを使ったサイト制作。</dd>
                     <dt>PHP</dt>
                     <dd>セブ島で2ヶ月学習。PHP7.3,Laravel5.7,XAMPを使用。お問い合わせフォーム、SNSを作成可能なレベル。</dd>
                     <dt>Java</dt>
-                    <dd>RaiseTechで4ヶ月学習(経緯はAWSを受講すると無料で受講できた為)。Java8/11, SpringBoot2.2.4, Thymeleaf, MyBatis, Jenkins, JUnit, Seleniumを使用。開発だけでなく、REST API,テストコード、自動化の基礎ができている状態。</dd>
+                    <dd>RaiseTechで4ヶ月学習。Java8/11, SpringBoot2.2.4, Thymeleaf, MyBatis, Jenkins, JUnit, Seleniumを使用。開発だけでなく、REST API,テストコード、自動化の基礎ができる。</dd>
                     <dt>Ruby</dt>
                     <dd>AWSでデプロイの練習をするために、軽く触れた程度。Qiitaを見ながら写真投稿SNS作成してEC2にデプロイ。Ruby2.6.4 ,Rails5.2.3を使用。</dd>
                     <dt>MySQL</dt>
                     <dd>5系,8系を使用。基本的なデータベース操作可能。簡単なSNSレベルのテーブル設計。</dd>
                     <dt>Network</dt>
-                    <dd>現在の職場で、学習中。主に、Cisco製品のL3,L2,PoEやSNMPによる監視。</dd>
+                    <dd>現在の職場で経験中。主にCisco製品のL3,L2,PoEの設定や監視。ネットワーク入門書で学習中。</dd>
                     <dt>Server</dt>
                     <dd>現在の職場では、Tomcat,CTI連携用サーバ(コンピュータと電話の連携)の運用保守。AWSのEC2を使用したサーバ構築(nginx/Unicorn, Tomcat/Appach)</dd>
                     <dt>Dorcker</dt>
-                    <dd>まだまだ学習中。DorkerでJavaとVScodeの開発環境、Tomcat環境を試しに使う程度。</dd>
+                    <dd>学習中。DorkerでJavaとVScodeの開発環境、Tomcat環境を試しに使う程度。</dd>
                     <dt>AWS</dt>
-                    <dd>RaiseTechで4ヶ月受講。現在は、Udemyで認定ソリューションアーキテクトを学習中。EC2/VPC/RDS/S3/ELB/Lambdaなどなど。上記のJavaとRubyのデプロイ環境はAWSで作成。CloudFormation/Ansible/Jenkinsでインフラ環境の自動化とコード化が可能。</dd>
+                    <dd>RaiseTechで4ヶ月受講。現在は、Udemyの講座で認定ソリューションアーキテクトを学習中。EC2/VPC/RDS/S3/ELB/Lambdaなどなど。JavaとRubyのデプロイをAWSで構築。CloudFormation/Ansible/Jenkinsでインフラ環境の自動化とコード化が可能。</dd>
                 </Fade>
             </dl>
         )             
--- src/components/Work.js
+++ src/components/Work.js
@@ -10,7 +10,7 @@ class Work extends React.Component {
                         <h3>ビル管理</h3>
                         <p>
                             <Fade bottom>
-                                静岡で総合ビルメンテナンス業に従事する。商業施設やオフィスビル、ホテル等の設備・清掃・警備を管理する。自ら点検・補修を行うこともあれば、工事の受託提案も行う。
+                                静岡で総合ビルメンテナンス業に従事する。商業施設やオフィスビル、ホテル等の設備・清掃・警備を管理する。自ら点検・補修を行うこともあれば、工事の見積提案、受託等も行う。
                             </Fade>
                         </p>
                     </div>
@@ -19,16 +19,16 @@ class Work extends React.Component {
                         <h3>セブ島留学</h3>
                         <p>
                             <Fade bottom>
-                                フィリピンのセブ島でエンジニア留学を経験する。午前中に英語、午後にプログラミンを学ぶ。HTML、CSS、JavaScript、PHPの基礎を学び、Webサイト制作とWebサービスの開発を学習する。
+                                フィリピンのセブ島でエンジニア留学を経験する。午前中に英語、午後にプログラミンを学ぶ。HTML、CSS、JavaScript、PHPの基礎を学び、Webサイト制作とWebサービス開発を学習する。
                             </Fade>
                         </p>
                     </div>
 
-                    <div class="timeline-item" date-is='2019年7月~2019年8月'>
+                    <div class="timeline-item" date-is='2019年7月~2019年9月'>
                         <h3>転職活動</h3>
                         <p>
                             <Fade bottom>
-                                大阪で転職活動を行う。この時期にインフラ・クラウド技術に興味を持ち、9月からRaiseTechでAWSの学習を行う。
+                                大阪で転職活動を行う。この時期にインフラ・クラウド技術に興味を持ち、9月から12月までRaiseTechでAWSとJavaの講座を受講する。
                             </Fade>
                         </p>
                     </div>
